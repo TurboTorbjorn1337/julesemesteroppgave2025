e2c4fbefcd237e020f359e21d8385e15e3e67d9e
Jørgen Jacobsen <jorgeja@gmail.com>
2025-10-27 13:53:57 +0100

    jobber oppgave

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+document.getElementById("navneskjema").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const firstname = document.getElementById("firstname").innerText;
+  let middlename = document.getElementById("middlename").innerText;
+  const lastname = document.getElementById("lastname").innerText;
+  middlename = "mikropenis";
+  document.getElementById(
+    "full-name"
+  ).innerText = `${firstname} ${middlename} ${lastname}`;
+});
